8d013da0fff6be8ee4d12ee1492db29747ae6bb2
Vladimir Solovey <vladimirsolovey@github.com>
2014-02-03 11:36:42 -0600

    Misc updates

--- Leverager/Controllers/HomeController.cs
+++ Leverager/Controllers/HomeController.cs
@@ -43,12 +43,6 @@ namespace Leverager.Controllers
             ViewBag.Title = "FAQ";
 
             var questions = new ModelContext().FAQSet.ToList();
-            /*{
-
-                new FAQ { Id = 1, Question = "Lorem ipsum dolor sit amet?", Answer = "Cras facilisis euismod mollis. Suspendisse potenti. Suspendisse rutrum commodo nisl id ultricies. Nam consequat molestie ipsum quis euismod." },
-                new FAQ { Id = 2, Question = "Mauris at lacus eu lectus pharetra varius?", Answer = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum." },
-                new FAQ { Id = 3, Question = "Nulla vitae dolor enim, vel adipiscing tortor?", Answer = "Quisque enim odio, mollis nec rutrum ac, blandit eu massa." }
-            };*/
 
             return View(questions);
         }
@@ -79,9 +73,9 @@ namespace Leverager.Controllers
                 }
 
             }
-            catch (DataException dex)
+            catch (DataException ex)
             {
-                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator." + ex.Message);
             }
             return View();
         }
